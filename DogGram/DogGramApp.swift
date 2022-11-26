b0a9ce8a9e58e02f7e593956d5eb15d850793961
mcantop <maciejcantop@icloud.com>
2022-11-26 19:38:13 +0100

    Initial Commit

---
+++ DogGram/DogGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogGramApp.swift
+//  DogGram
+//
+//  Created by Maciej on 26/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct DogGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
